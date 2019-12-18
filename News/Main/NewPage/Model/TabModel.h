ea5151d901cd28cd17b3c044113cbbb3db938c9a
tplish <1372638698@qq.com>
2019-12-18 12:30:57 +0800

    basic-tool

---
+++ News/Main/NewPage/Model/TabModel.h
@@ -0,0 +1,17 @@
+//
+//  TabModel.h
+//  News
+//
+//  Created by tplish on 2019/12/18.
+//  Copyright © 2019 Team09. All rights reserved.
+//
+
+#ifndef TabModel_h
+#define TabModel_h
+
+@interface TabModel : NSObject
+@property (nonatomic) NSInteger len;
+@property (nonatomic, strong) NSArray * names;
+@end
+
+#endif /* TabModel_h */
