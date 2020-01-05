0df442d7f306b2662fec55c9b34189ea606441ed
tplish <1372638698@qq.com>
2020-01-05 18:17:37 +0800

    fix: BaseIP

--- K+News/Main/Base/GlobalVariable.m
+++ K+News/Main/Base/GlobalVariable.m
@@ -10,8 +10,6 @@
 #import "GlobalVariable.h"
 
 NSString * const PrivateIP = @"http://localhost";
-NSString * const PublicIP = @"http://47.102.157.223";
-NSString * const PublicIP2 = @"http://193.112.252.88";
 
-NSString * const BaseIP = PublicIP2;
+NSString * const BaseIP = PrivateIP;
 
